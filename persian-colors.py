5e4a49f2b753093262e8c793636c524f80f99a3b
Max Base <MaxBaseCode@Gmail.Com>
2021-11-11 12:00:20 +0330

    Create  persian-colors.py

---
+++ persian-colors.py
@@ -0,0 +1,11 @@
+persianColors = {
+    "red"        : "#c81d11",
+    "plum"       : "#701c1c",
+    "pink"       : "#f77fbe",
+    "rose"       : "#fe28a2",
+    "green"      : "#00a693",
+    "orange"     : "#d99058",
+    "indigo"     : "#32127a",
+    "blue"       : "#1c39bb",
+    "mediumBlue" : "#0067a5",
+}
